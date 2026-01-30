9a5b787a83456a5e83b530ab867e6897ff54e2e8
shji47603 <shji47603@gmail.com>
2026-01-30 15:56:31 +0300

    fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,8 +3,6 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from './models/user.model';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtGuard } from '../auth/guards/auth.guard';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
 
